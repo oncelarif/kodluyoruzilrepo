4e0254386eaf0c3d5cda178461c9b28e8e26bac8
oncelarif <109680012+oncelarif@users.noreply.github.com>
2023-02-13 23:21:59 +0300

    Add files via upload

---
+++ form/kontrol.php
@@ -0,0 +1,8 @@
+<?php 
+$adsoyad = $_POST ["adsoyad"];
+$email= $_POST ["email"];
+$sifre=$_POST["sifre"];
+
+echo "Merhaba,<h2>Kayıt İşleminiz Tamamlandı.</h2><hr>İsminiz: ".$adsoyad."<br>E-Mail Adresiniz: ".$email."<br>Şifreniz: ".$sifre;
+
+?>
